998ca60376fec583c85ed6097cbaa08072a390d0
edwar moran <edwar.moran.12@gmail.com>
2023-06-28 19:33:07 -0500

    chore: se agrego clean de datasource y repository

---
+++ lib/domain/datasource/video_post_datasource.dart
@@ -0,0 +1,10 @@
+
+import 'package:toktik/domain/entities/video_post.dart';
+
+abstract class VideoPostDataSource {
+
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId);
+
+  Future<List<VideoPost>> getTrendingVideosByPage(int page);
+
+}
---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,26 @@
+
+
+import 'package:toktik/domain/datasource/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepositoryImpl implements VideoPostRepository {
+
+  final VideoPostDataSource videosDataSource;
+
+  VideoPostsRepositoryImpl({
+    required this.videosDataSource
+  });
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userId) {
+    // TODO: implement getFavoriteVideosByUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDataSource.getTrendingVideosByPage(page);
+  }
+
+}
